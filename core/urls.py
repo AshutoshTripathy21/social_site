1f58c28a11d6acdf33e4fc987333d9f8dacd2791
AshutoshTripathy21 <ashutoshtripathy408@gmail.com>
2024-05-05 17:09:33 +0530

    Minor updates in backend

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('signup', views.signup, name='signup')
+    path('signup', views.signup, name='signup'),
+    path('signin', views.signin, name='signin'),
+    path('logout', views.logout, name='logout'),
 ]
--- core/views.py
+++ core/views.py
@@ -40,3 +40,21 @@ def signup(request):
             return redirect('signup')
     else:
         return render(request, 'signup.html')
+    
+def signin(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'Credentials Invalid')
+            return redirect('signin')
+    return render(request, 'signin.html')
+
+def logout(request):
+    auth.logout(request)
+    return redirect('signin')
