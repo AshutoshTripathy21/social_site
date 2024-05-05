0bcd4514245d632a21f78626df8c92aac02762c2
AshutoshTripathy21 <ashutoshtripathy408@gmail.com>
2024-05-05 16:29:43 +0530

    first commit for social app

---
+++ core/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.models import User, auth
+from django.http import HttpResponse
+from .models import *
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def signup(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+
+        if password == password2:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'Email already exists')
+                return redirect('signup')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'Username is already taken')
+                return redirect('signup')
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password)
+                user.save()
+
+                #Log user in and redirect in settings page
+
+                #create a profile object for the new user
+                user_model = User.objects.get(username=username)
+                new_profiel = Profile.objects.create(user=user_model, id_user=user_model.id)
+                new_profiel.save()
+                return redirect('index')
+
+        else:
+            messages.info(request, "Password don't match")
+            return redirect('signup')
+    else:
+        return render(request, 'signup.html')
